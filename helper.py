d0318641ee0fe9ffbd59eae2050e2d5af080db71
qcq <qcq@quocs-mbp.attlocal.net>
2015-05-25 21:19:23 -0700

    Second submission, linear reg

---
+++ helper.py
@@ -0,0 +1,58 @@
+import os
+import numpy as np
+import pandas as pd
+from pandas.io.parsers import read_csv
+from sklearn.utils import shuffle
+
+
+FTRAIN = '~/kaggle/data/training.csv'
+FTEST = '~/kaggle/data/test.csv'
+
+
+def load(test=False, cols=None):
+    """Loads data from FTEST if *test* is True, otherwise from FTRAIN.
+    Pass a list of *cols* if you're only interested in a subset of the
+    target columns.
+    """
+    fname = FTEST if test else FTRAIN
+    df = read_csv(os.path.expanduser(fname))  # load pandas dataframe
+
+    # The Image column has pixel values separated by space; convert
+    # the values to numpy arrays:
+    df['Image'] = df['Image'].apply(lambda im: np.fromstring(im, sep=' '))
+
+    if cols:  # get a subset of columns
+        df = df[list(cols) + ['Image']]
+
+    #print(df.count())  # prints the number of values for each column
+    df = df.dropna()  # drop all rows that have missing values in them
+
+    X = np.vstack(df['Image'].values) / 255.  # scale pixel values to [0, 1]
+    X = X.astype(np.float32)
+
+    if not test:  # only FTRAIN has any target columns
+        y = df[df.columns[:-1]].values
+        y = (y - 48) / 48  # scale target coordinates to [-1, 1]
+        X, y = shuffle(X, y, random_state=42)  # shuffle train data
+        y = y.astype(np.float32)
+    else:
+        y = None
+
+    return X, y
+
+if __name__ == '__main__':
+    X, y = load()
+    print("X.shape == {}; X.min == {:.3f}; X.max == {:.3f}".format(
+        X.shape, X.min(), X.max()))
+    print("y.shape == {}; y.min == {:.3f}; y.max == {:.3f}".format(
+        y.shape, y.min(), y.max()))
+
+def submit(yhat_test_form,subname):
+    idlook = '/Users/qcq/kaggle/data/IdLookupTable.csv'
+    sub = pd.DataFrame.from_csv(idlook)
+    
+    cols = ['left_eye_center_x','left_eye_center_y','right_eye_center_x','right_eye_center_y','left_eye_inner_corner_x','left_eye_inner_corner_y','left_eye_outer_corner_x','left_eye_outer_corner_y','right_eye_inner_corner_x','right_eye_inner_corner_y','right_eye_outer_corner_x','right_eye_outer_corner_y','left_eyebrow_inner_end_x','left_eyebrow_inner_end_y','left_eyebrow_outer_end_x','left_eyebrow_outer_end_y','right_eyebrow_inner_end_x','right_eyebrow_inner_end_y','right_eyebrow_outer_end_x','right_eyebrow_outer_end_y','nose_tip_x','nose_tip_y','mouth_left_corner_x','mouth_left_corner_y','mouth_right_corner_x','mouth_right_corner_y','mouth_center_top_lip_x','mouth_center_top_lip_y','mouth_center_bottom_lip_x','mouth_center_bottom_lip_y','Image']
+    
+    sub['Location'] = sub.apply(lambda row: yhat_test_form[row['ImageId']-1,cols.index(row['FeatureName'])],axis=1)
+    
+    sub['Location'].to_csv(subname,header=True)
